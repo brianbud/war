679f855e5da3f5cf9d25aa2768841d5b69def89e
Brian Bud <hello@brianbud.com>
2024-01-10 21:43:42 -0500

    display cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let newDeckBtn = document.querySelector('#new-deck');
 let drawBtn = document.querySelector('#draw');
+let cards = document.querySelector('#cards');
 
 newDeckBtn.addEventListener('click', handleClick);
 drawBtn.addEventListener('click', drawCards);
@@ -24,6 +25,20 @@ function drawCards() {
     .then((data) => {
       p1Card = data.cards[0];
       p2Card = data.cards[1];
+      displayCards();
       console.log('player 1 card', p1Card, 'player2 card', p2Card);
     });
+}
+
+function displayCards() {
+  cards.innerHTML = `
+    <div>
+        <h2>Player 1</h2>
+        <img src="${p1Card.image}">
+    </div>
+    <div>
+        <img src="${p2Card.image}">
+        <h2>Player 2</h2>
+    </div>
+  `;
 }
