15e3cabed6163f02d646d5c59abbd1f3a8b79cc5
Brian Bud <hello@brianbud.com>
2024-01-10 21:11:08 -0500

    make sure deck of card api is working

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+let btn = document.querySelector('#new-deck');
+btn.addEventListener('click', handleClick);
+
+function handleClick() {
+  fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle')
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
