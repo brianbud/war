c76c46945e70b4cb3ca2376c603a46b012f4ea6d
Brian Bud <hello@brianbud.com>
2024-01-10 21:21:09 -0500

    draw 2 cards from deck

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-let btn = document.querySelector('#new-deck');
-btn.addEventListener('click', handleClick);
+let newDeckBtn = document.querySelector('#new-deck');
+let drawBtn = document.querySelector('#draw');
+
+newDeckBtn.addEventListener('click', handleClick);
+drawBtn.addEventListener('click', drawCards);
+
+let deckId;
+let p1Card;
+let p2Card;
 
 function handleClick() {
   fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle')
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+      console.log(deckId);
+    });
+}
+
+function drawCards() {
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      p1Card = data.cards[0];
+      p2Card = data.cards[1];
+      console.log('player 1 card', p1Card, 'player2 card', p2Card);
+    });
 }
